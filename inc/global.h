ad145c10e3b32d6f0182e8a7f286f1915632668f
raskolnikof <raskolnikof@neif.neif>
2025-03-26 12:50:54 +0100

    struct ether_arp is all we need. check whats going on when printing hwaddr. also added some globals

--- inc/global.h
+++ inc/global.h
@@ -2,6 +2,10 @@
 #define GLOBAL_H
 
 #define ETH_HDR_LEN 14
+#define EA_HDR_LEN 8
+#define PROT_ADRESS 4
+#define TWO 2
+#define ONE 1
 
 typedef struct {
 	int raw_socket;
--- src/main.c
+++ src/main.c
@@ -29,9 +29,10 @@ int fatal_error()
 int main()
 {
 	struct	sigaction	sigact;
-	char			buf[ETH_HDR_LEN];
+	int			buf_size = EA_HDR_LEN + ETH_ALEN * TWO + PROT_ADRESS * TWO;
+	char			buf[buf_size + ONE];
 	int			n_bytes_readen;
-	struct	arphdr		arp_header;
+	struct	ether_arp	etharp;
 
 	n_bytes_readen = 0;
 
@@ -39,23 +40,30 @@ int main()
 	sigact.sa_handler = program_interruption;
 	sigaction(SIGINT, &sigact, NULL);
 
+	//(void)eth_header;
+	//printf("tamanio: %lu\n", sizeof(struct	ether_arp));
 	data.go_on = 1;
 	if ((data.raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP))) < 0)
 		return (fatal_error());
 
-	memset((struct arphdr *)&arp_header, 0, sizeof(arp_header));
+	//etharp = NULL;
+	//memset((struct ethhdr *)eth_header, 0, sizeof(struct ethhdr ));
 	memset(buf, 0, sizeof(char));
 	while (data.go_on > 0)
 	{
-		n_bytes_readen = recvfrom(data.raw_socket, buf, ETH_HDR_LEN + 1, 0, NULL, 0);
+		n_bytes_readen = recvfrom(data.raw_socket, buf, buf_size, 0, NULL, 0);
 		printf("n_bytes: %d\n", n_bytes_readen);
 		if (n_bytes_readen < 0)
 			return (fatal_error());
 		else
 		{
 			buf[n_bytes_readen] = 0;
-			memcpy(&arp_header, buf, n_bytes_readen);
-			printf("h_dest: %d\nh_source: %d\n", arp_header.ar_hrd, arp_header.ar_pro);
+			memcpy(&etharp, buf, sizeof(etharp));
+			//etharp = (struct ether_arp )buf;
+			//size_t var_size = sizeof(etharp.ea_hdr);
+			//printf("joder ya estamos como chempre :)) : %lu\n", var_size);
+			printf("adressida del sender: %x:%x:%x:%x:%x:%x\n", ((&etharp + 8)->arp_sha[0]), ((etharp).arp_sha[1]),((etharp).arp_sha[2]),((etharp).arp_sha[3]), ((etharp).arp_sha[4]),((etharp).arp_sha[5]));
+			//printf("h_dest: %d\nh_source: %d\n", (eth_header.dst), ((eth_header + 6).src));
 			data.go_on = -1;
 		}
 	}
